f03e7fc15f78680dbdc1802d61f9ec0b9f864fb3
brandongatlin <brandongatlin1981@me.com>
2018-06-10 18:52:46 -0500

    moved key

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
-const API_KEY = 'AIzaSyDxfFtYj-CN2S-rz_4aarRDyTJUW_PwgH8';
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 
 class App extends Component {
